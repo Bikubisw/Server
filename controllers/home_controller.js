eab9874de029875c5227fdff48c3dad4afd6ed2f
Bikram Biswas <bikrambiswas043@gmail.com>
2020-05-18 02:36:05 +0530

    add a router and controller

---
+++ controllers/home_controller.js
@@ -0,0 +1,6 @@
+module.exports.home = function(req, res) {
+    return res.end("<h1>My name is Bikram Biswas</h1>");
+}
+module.exports.action = function(req, res) {
+    return res.end("<h1>What the fuck bitch</h1>")
+}
