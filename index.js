099ca151deee18a9271fd3c5f6cc09b500fbc53a
Bikram Biswas <bikrambiswas043@gmail.com>
2020-05-26 03:08:23 +0530

    All the layouts and static files are made

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const app = express();
 const port = 8000;
 const path = require('path');
+const expressLayout = require("express-ejs-layouts");
 // use express router
+app.use(expressLayout);
 app.use(require('./routes'));
 app.set("view engine", "ejs");
+app.use(express.static('assets'));
+app.use(expressLayout);
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
+
 app.set("views", path.join(__dirname, 'views'));
 app.listen(port, (err) => {
     if (err) {
